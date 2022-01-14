29cfb987799fa33e5ee5515b15109372fe5d7df4
Will Tonkin <git@willtonkin.com>
2022-01-14 17:04:14 +0100

    tests: increase test coverage
    we've gone from 63% line coverage to 90%

--- src/__tests__/tests.ts
+++ src/__tests__/tests.ts
@@ -21,6 +21,26 @@ describe("reactFromHtml E2E tests", async () => {
     expect(documentElement.innerHTML).toMatchSnapshot();
   });
 
+  it("Should not re-hydrate already hydrated components on multiple `reactFromHtml()` calls", async () => {
+    const componentName: string = "myComponent";
+
+    const mockCall = jest.fn();
+    const hydrator = async () => {
+      mockCall();
+
+      return React.createElement("span", {}, "hydrated component");
+    };
+
+    const hydrators = { [`.${componentName}`]: hydrator };
+    const documentElement = document.createElement("div");
+
+    documentElement.innerHTML = `<div class="${componentName}"></div>`;
+
+    await reactFromHtml(documentElement, hydrators);
+    await reactFromHtml(documentElement, hydrators);
+    expect(mockCall).toBeCalledTimes(1);
+  });
+
   it("Should work for nested hydratables", async () => {
     const mockCall = jest.fn();
     const hydrators: IHydrator = {
@@ -70,5 +90,59 @@ describe("reactFromHtml E2E tests", async () => {
     });
 
     expect(documentElement.innerHTML).toMatchSnapshot();
+  });
+
+  it("Should not mutate static markup", async () => {
+    const componentName: string = "myComponent";
+
+    const hydrator = async () => {
+      return React.createElement("span", {}, "hydrated component");
+    };
+
+    const hydrators = { [`.${componentName}`]: hydrator };
+    const documentElement = document.createElement("div");
+
+    documentElement.innerHTML = `
+    <div>
+      <p>Static HTML, with some <b style="font-weight:bold;">interesting</b> markup</p>
+      <div data-hydratable="test-${componentName}"></div>
+    </div>`;
+
+    await reactFromHtml(documentElement, hydrators);
+
+    expect(documentElement.innerHTML).toMatchSnapshot();
+  });
+
+  it("Should not mutate static markup of nested children", async () => {
+    const mockCall = jest.fn();
+    const hydrators: IHydrator = {
+      [`.Parent`]: async (el: Element, hydrate) => {
+        mockCall();
+
+        return React.createElement("span", {}, await hydrate(el));
+      },
+    };
+
+    const documentElement = document.createElement("div");
+
+    documentElement.innerHTML = `
+      <div class="Parent">
+        <p>Static HTML, with some <b style="font-weight:bold;">interesting</b> markup</p>
+        <form>
+          <input type="checkbox" name="confirm" value="confirmed" checked>
+          <select id="favourite-color">
+            <option value="red">red</option>
+            <option value="green" selected>green</option>
+            <option value="blue">blue</option>
+          </select>
+          <textarea name="message">Default value</textarea>
+        </form>
+      </div>
+      `;
+
+    await reactFromHtml(documentElement, hydrators);
+
+    expect(documentElement.innerHTML).toMatchSnapshot();
+    expect(mockCall).toBeCalledTimes(1);
   });
 });
